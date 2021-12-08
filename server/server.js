06079ded202aae5f623d067fd04a8742ad7bf7b3
kurtjacob22 <kurtjacob.urquico@gmail.comb>
2021-12-08 19:59:52 +0800

    modified server

--- server/server.js
+++ server/server.js
@@ -23,20 +23,23 @@ app.post("/postreq", async (req, res, next) => {
 });
 
 app.post("/login", async (req, res, next) => {
-  const CHECK_QUERY = `SELECT email AND password FROM studentportal.userlogin WHERE email LIKE '${req.body.username}' AND password LIKE '${req.body.password}';`;
+  const CHECK_QUERY = `SELECT * FROM studentportal.userlogin WHERE email LIKE '${req.body.username}' AND password LIKE '${req.body.password}';`;
   console.log("data has been received");
   connection.query(CHECK_QUERY, (err, result) => {
     if (err) {
-      res.send(err);
+      res.send({ error: err });
     }
-    console.log(result);
-    // else {
-    //   res.send("The query is successful");
-    // }
+
+    if (result.length > 0) {
+      res.send({ message: `Hello ${req.body.username}!`, login: true });
+    } else {
+      res.send({ message: "Incorrect password/user!", login: false });
+    }
   });
   console.log(req.body);
 });
 
+//! PORT
 app.listen(4000, () => {
   console.log("server is working in port 4000");
 });
